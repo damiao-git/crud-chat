7043317367c98354bfcd81c2d888ed0e3f7b7c0e
damigaos <damiao.jose@hotmail.com>
2024-12-20 17:56:15 -0300

    finalizado

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -12,7 +12,9 @@ class TarefaController extends Controller
      */
     public function index()
     {
-        //
+        $tarefas = Tarefa::all();
+
+        return response()->json($tarefas);
     }
 
     /**
@@ -28,15 +30,31 @@ class TarefaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $tarefa = Tarefa::create($request->all());
+
+            return response()->json([$tarefa],201);
+        }
+        catch (\Exception $e) {
+            return response()->json(["error"=> $e->getMessage()],500);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Tarefa $tarefa)
+    public function show($id)
     {
-        //
+
+            //code...
+            $tarefa = Tarefa::find($id);
+            if($tarefa){
+                return response()->json([$tarefa],302);
+            }else{
+                return response()->json(["error"=> "Não encontrada"],404);
+            }
+
+
     }
 
     /**
@@ -50,16 +68,32 @@ class TarefaController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Tarefa $tarefa)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+            $tarefa = Tarefa::find($id);
+            if($tarefa){
+                $tarefa->update($request->all());
+                return response()->json([$tarefa],200);
+            }else{
+                return response()->json(["error"=> "Não encontrada"],404);
+            }
+        } catch (\Throwable $th) {
+            return response()->json(["error"=> $th->getMessage()],500);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Tarefa $tarefa)
+    public function destroy($id)
     {
-        //
+        $tarefa = Tarefa::find($id);
+        if($tarefa){
+            $tarefa->delete();
+            return response()->json([$tarefa],200);
+        }else{
+            return response()->json(["error"=> "Não encontrada"],404);
+        }
     }
 }
--- database/migrations/2024_12_20_202820_create_tarefas_table.php
+++ database/migrations/2024_12_20_202820_create_tarefas_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('tarefas', function (Blueprint $table) {
             $table->id();
+            $table->string('titulo');
+            $table->string('descricao');
+            $table->string('status');
             $table->timestamps();
         });
     }
